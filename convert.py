37984a14b2d05a37a30472db44329b676db03030
Nikhil yadav <cloud@nblik.com>
2023-06-27 18:15:26 +0530

    added initial code for staging env

---
+++ convert.py
@@ -0,0 +1,13 @@
+import base64 
+
+
+contents = open("temp.txt").read().splitlines()
+
+out = open("tempnew.txt","w")
+for x in contents:
+    obj = x.split(":")
+    a = obj[0]
+    b = base64.b64encode(obj[1].replace('"','').encode()).decode()
+    out.write(a+": "+b)
+    out.write("\n")
+out.close()
